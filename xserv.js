bf9011a50dd599c71492da9b56e0e1674b6beed0
Giovanni Amati <giovanni.amati@messagenet.it>
2015-11-16 18:46:58 +0100

    fix

--- xserv.js
+++ xserv.js
@@ -1,10 +1,11 @@
-(function() { 
+(function() {
     var Xserv = function(app_id) {
 	var BIND = 100;
 	var UNBIND = 101;
 	var HISTORY = 102;
 	var HISTORY_ID = 'id';
 	var HISTORY_TIMESTAMP = 'timestamp';
+	var OP_SEP = ':';
 	
 	this.app_id = app_id;
 	this.url = 'ws://localhost:4321/ws';
@@ -19,27 +20,35 @@
 	    return arguments.callee._singletonInstance;
 	}
 	arguments.callee._singletonInstance = this;
-	// end
+	//
 	
 	this.addEventListener = function(name, callback) {
 	    if (name == 'message') {
 		
 	    } else if (name == 'events') {
 		var event_callback = function(event) {
-		    var ev = JSON.parse(event.data);
-		    if (!ev.op) {
+		    // intercetta solo i messaggi
+		    if (event.data.charAt(0) != OP_SEP) {
+			var ev = JSON.parse(event.data);
 			callback(ev.id, ev.name, ev.topic, ev.message, ev.timestamp);
 		    }
-		};
+		}.bind(this);
 		
 		this.listeners.push({event: 'message', callback: event_callback});
-	    } else if (name == 'op_succeeded') {
+	    } else if (name == 'events:op') {
 		var event_callback = function(event) {
-		    var ev = JSON.parse(event.data);
-		    if (ev.op) {
-			callback(ev.op, ev.topic, ev.event);
+		    // intercetta solo gli event dei comandi
+		    if (event.data.charAt(0) == OP_SEP) {
+			var ev = event.data.split(OP_SEP);
+			if (ev.length >= 7) {
+			    var base64 = ev[4];
+			    var dec = atob(base64);
+			    var data2 = jQuery.parseJSON(dec);
+			    var data = dec;
+			    callback(this.stringifyOpCode(ev[1]), ev[2], ev[3], data, ev[5] == 'true', ev[6]);
+			}
 		    }
-		};
+		}.bind(this);
 		
 		this.listeners.push({event: 'message', callback: event_callback});
 	    } else {
@@ -93,15 +102,25 @@
 	    }
 	};
 	
+	this.stringifyOpCode = function(code) {
+	    if (code == BIND) {
+		return 'bind';
+	    } else if (code == UNBIND) {
+		return 'unbind';
+	    } else if (code == HISTORY) {
+		return 'history';
+	    }
+	};
+	
 	this.setReconnectInterval = function(value) {
 	    this.reconnectInterval = value;
 	};
 	
 	this.bind = function(topic, event) {
 	    this.ops.push({app_id: this.app_id, 
-			     op: BIND, 
-			     topic: topic, 
-			     event: event});
+			   op: BIND, 
+			   topic: topic, 
+			   event: event});
 	};
 	
 	this.unbind = function(topic, event) {
@@ -111,6 +130,24 @@
 			op: UNBIND, 
 			topic: topic, 
 			event: event});
+	};
+	
+	this.historyById = function(topic, event, value) {
+	    this.ops.push({app_id: this.app_id, 
+			   op: HISTORY, 
+			   topic: topic, 
+			   event: event,
+			   arg1: HISTORY_ID,
+			   arg2: String(value)});
+	};
+	
+	this.historyByTimestamp = function(topic, event, value) {
+	    this.ops.push({app_id: this.app_id, 
+			   op: HISTORY, 
+			   topic: topic, 
+			   event: event,
+			   arg1: HISTORY_TIMESTAMP.toString(),
+			   arg2: String(value)});
 	};
     };
     
