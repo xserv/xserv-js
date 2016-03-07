08e21b0439dd358fcf4302068dca8d3008778b6e
Giovanni Amati <amatig@gmail.com>
2016-03-07 16:58:55 +0100

    fix

--- xserv.js
+++ xserv.js
@@ -518,8 +518,8 @@
 	
 	Xserv.VERSION = '1.0.0';
 	
-	Xserv.HOST = '192.168.130.187';
-	// Xserv.HOST = 'mobile-italia.com';
+	// Xserv.HOST = '192.168.130.187';
+	Xserv.HOST = 'mobile-italia.com';
 	Xserv.PORT = '4332';
 	Xserv.TLS_PORT = '8332';
 	Xserv.WS_URL = 'ws$1://$2:$3/ws/$4?version=$5';
